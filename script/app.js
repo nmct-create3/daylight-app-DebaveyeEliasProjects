0ebbdb4e5affb0b25b55c3a867d386d695398c15
DebaveyeEliasProjects <55786829+DebaveyeEliasProjects@users.noreply.github.com>
2020-10-15 20:00:41 +0200

    kinda works, but need some globalisation. Page only wanna work in belgium, fix soon. hope

--- script/app.js
+++ script/app.js
@@ -1,51 +1,64 @@
-// _ = helper functions
-function _parseMillisecondsIntoReadableTime(timestamp) {
-	//Get hours from milliseconds
-	const date = new Date(timestamp * 1000);
-	// Hours part from the timestamp
-	const hours = '0' + date.getHours();
-	// Minutes part from the timestamp
-	const minutes = '0' + date.getMinutes();
-	// Seconds part from the timestamp (gebruiken we nu niet)
-	// const seconds = '0' + date.getSeconds();
 
-	// Will display time in 10:30(:23) format
-	return hours.substr(-2) + ':' + minutes.substr(-2); //  + ':' + s
+const showResult = function(json) {
+	console.log(json);
+	let sunrise = json.city.sunrise + json.city.timezone;
+	let sunset = json.city.sunset + json.city.timezone;
+	let date = new Date();
+	let time1 = date.getTime()/1000;
+
+	let time =(time1 *1000)+ json.city.timezone;
+	let percentage = 100 -((sunset - sunrise)/time)*100;
+	console.error(percentage)
+	let perc = ((sunset - sunrise)/time)*100
+	console.error(perc)
+	console.log(sunrise);
+	console.log(sunset);
+	console.log(time);
+	document.querySelector(`.js-sun`).style.bottom = `${perc}%`;
+	document.querySelector(`.js-sun`).style.left =  `${percentage}%`;
+	let time2 = convertTImne(((date.getTime())/1000)+ json.city.timezone);
+	document.querySelector(`.js-sun`).setAttribute("data-time",time2);
+	document.querySelector(".js-sunrise").innerHTML = convertTImne(sunrise);
+	document.querySelector(".js-sunset").innerHTML = convertTImne(sunset);
+	let dateSunset = toDate(sunset);
+	let dateSunrise = toDate(sunrise);
+	if(date.getTime() > dateSunset.getTime() && date.getTime() > dateSunrise.getTime()){
+		document.querySelector(".is-day").classList.add("is-night");
+	}
+	else{
+		document.querySelector(".is-day").classList.remove("is-night");
+	}
+};
+
+const toDate = function(millis){
+	return new Date(millis * 1000);
 }
 
-// 5 TODO: maak updateSun functie
+const convertTImne = function(millis){
+	let d = new Date(millis*1000);
+	hours = format_two_digits(d.getUTCHours());
+    minutes = format_two_digits(d.getUTCMinutes());
+    seconds = format_two_digits(d.getSeconds());
+    return hours + ":" + minutes;
+}
+const format_two_digits = function(n) {
+    return n < 10 ? '0' + n : n;
+}
 
-// 4 Zet de zon op de juiste plaats en zorg ervoor dat dit iedere minuut gebeurt.
-let placeSunAndStartMoving = (totalMinutes, sunrise) => {
-	// In de functie moeten we eerst wat zaken ophalen en berekenen.
-	// Haal het DOM element van onze zon op en van onze aantal minuten resterend deze dag.
-	// Bepaal het aantal minuten dat de zon al op is.
-	// Nu zetten we de zon op de initiële goede positie ( met de functie updateSun ). Bereken hiervoor hoeveel procent er van de totale zon-tijd al voorbij is.
-	// We voegen ook de 'is-loaded' class toe aan de body-tag.
-	// Vergeet niet om het resterende aantal minuten in te vullen.
-	// Nu maken we een functie die de zon elke minuut zal updaten
-	// Bekijk of de zon niet nog onder of reeds onder is
-	// Anders kunnen we huidige waarden evalueren en de zon updaten via de updateSun functie.
-	// PS.: vergeet weer niet om het resterend aantal minuten te updaten en verhoog het aantal verstreken minuten.
-};
-
-// 3 Met de data van de API kunnen we de app opvullen
-let showResult = queryResponse => {
-	// We gaan eerst een paar onderdelen opvullen
-	// Zorg dat de juiste locatie weergegeven wordt, volgens wat je uit de API terug krijgt.
-	// Toon ook de juiste tijd voor de opkomst van de zon en de zonsondergang.
-	// Hier gaan we een functie oproepen die de zon een bepaalde positie kan geven en dit kan updaten.
-	// Geef deze functie de periode tussen sunrise en sunset mee en het tijdstip van sunrise.
-};
-
-// 2 Aan de hand van een longitude en latitude gaan we de yahoo wheater API ophalen.
 let getAPI = (lat, lon) => {
-	// Eerst bouwen we onze url op
-	// Met de fetch API proberen we de data op te halen.
-	// Als dat gelukt is, gaan we naar onze showResult functie.
+
+	let url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=5c52657d3beb0a23dfe758162c224a9c&units=metric&lang=nl&cnt=1`;
+    const endpoint = url;
+    fetch(endpoint).then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        console.log(json);
+        showResult(json);
+    }).catch(function(error) {
+        console.error('An error occured, we handled it.', error);
+    });
 };
 
 document.addEventListener('DOMContentLoaded', function() {
-	// 1 We will query the API with longitude and latitude.
-	getAPI(50.8027841, 3.2097454);
+	getAPI(40.730610,-73.935242);
 });
